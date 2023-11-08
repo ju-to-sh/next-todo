cb66ba9dfa5f2fac67e57507eb75d440d067f3c9
ju-to-sh <088t3748@gmail.com>
2023-11-09 06:49:33 +0900

    削除機能の実装

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { PrimaryButton } from "@/components/atoms/PrimaryButton";
 import { SecondaryButton } from "@/components/atoms/SecondaryButton";
-import { Heading, Flex, Input, Box, ListItem, UnorderedList, Text } from "@chakra-ui/react";
+import { Heading, Flex, Input, Box, ListItem, UnorderedList, Text, StackDivider, VStack } from "@chakra-ui/react";
 import { useState } from "react";
 
 export default function Home() {
@@ -13,41 +13,48 @@ export default function Home() {
     setIncompleteTodos(newTodos);
     setTodoText("");
   };
+  const onClickDeleteTodo = (index: number) => {
+    const newTodos = [...incompleteTodos];
+    newTodos.splice(index, 1);
+    setIncompleteTodos(newTodos);
+  };
 
   return (
     <main>
       <Heading as="h1" size="xl" textAlign="center" p="4">
         To do
       </Heading>
-      <Box padding="4" color="black" maxW="md" bg="#eee" minWidth="400px">
-        <Heading as="h2" size="md" mb="4" textAlign="center">
-          タスク登録
-        </Heading>
-        <Flex alignItems="center">
-          <Input placeholder="タスクを入力" value={todoText} onChange={onChangeTodoText} borderColor="black" focusBorderColor="lime" borderRadius="4" size="sm" p="2" mr="4" />
-          <PrimaryButton onClick={onClickAddTodo}>追加</PrimaryButton>
-        </Flex>
-      </Box>
-      <Box padding="4" color="black" maxW="md">
-        <Heading as="h2" size="md" mb="4" textAlign="center">
-          未完了のタスク
-        </Heading>
-        <UnorderedList>
-          {incompleteTodos.map((todo, index) => {
-            return (
-              <ListItem key={index} display="flex" justifyContent="center" p="1">
-                <Flex alignItems="center">
-                  <Text fontSize="sm" mr="4">
-                    {todo}
-                  </Text>
-                  <PrimaryButton onClick={onClickAddTodo}>完了</PrimaryButton>
-                  <SecondaryButton onClick={onClickAddTodo}>削除</SecondaryButton>
-                </Flex>
-              </ListItem>
-            );
-          })}
-        </UnorderedList>
-      </Box>
+      <VStack divider={<StackDivider borderColor="gray.200" />} spacing={4} align="stretch">
+        <Box padding="4" color="black" maxW="md" bg="#eee" minWidth="400px">
+          <Heading as="h2" size="md" mb="4" textAlign="center">
+            タスク登録
+          </Heading>
+          <Flex alignItems="center">
+            <Input placeholder="タスクを入力" value={todoText} onChange={onChangeTodoText} borderColor="black" focusBorderColor="lime" borderRadius="4" size="sm" p="2" mr="4" />
+            <PrimaryButton onClick={onClickAddTodo}>追加</PrimaryButton>
+          </Flex>
+        </Box>
+        <Box padding="4" color="black" maxW="md">
+          <Heading as="h2" size="md" mb="4" textAlign="center">
+            未完了のタスク
+          </Heading>
+          <UnorderedList>
+            {incompleteTodos.map((todo, index) => {
+              return (
+                <ListItem key={index} display="flex" justifyContent="center" p="1">
+                  <Flex alignItems="center">
+                    <Text fontSize="sm" mr="4">
+                      {todo}
+                    </Text>
+                    <PrimaryButton onClick={onClickAddTodo}>完了</PrimaryButton>
+                    <SecondaryButton onClick={() => onClickDeleteTodo(index)}>削除</SecondaryButton>
+                  </Flex>
+                </ListItem>
+              );
+            })}
+          </UnorderedList>
+        </Box>
+      </VStack>
     </main>
   );
 }
