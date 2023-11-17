309853d3b5d27bfba1da05a7955c908da483b5e3
ju-to-sh <088t3748@gmail.com>
2023-11-18 08:13:34 +0900

    ローディング中のアイコンおよびアニメーションの追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Providers } from "../providers/Providers";
 import { Container } from "@chakra-ui/react";
-import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Create Next App",
--- src/components/organisms/WeatherInfo.tsx
+++ src/components/organisms/WeatherInfo.tsx
@@ -1,4 +1,5 @@
-import { Table, Thead, Tbody, Tr, Th, Td, TableCaption, TableContainer } from "@chakra-ui/react";
+import { Box, Text, Flex, Table, Thead, Tbody, Tr, Th, Td, TableCaption, TableContainer, keyframes } from "@chakra-ui/react";
+import { SpinnerIcon, SunIcon } from "@chakra-ui/icons";
 import { WeatherInfoType } from "../../app/types/WeatherInfoType";
 
 type Props = {
@@ -8,6 +9,16 @@ type Props = {
 const FirstTime = 7;
 const EndTime = 17;
 
+const spin = keyframes`
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  `;
+const spinAnimation = `${spin} infinite 1s linear`;
+
 const formatDate = (date: string) => {
   return new Date(date).getHours();
 };
@@ -15,10 +26,17 @@ const formatDate = (date: string) => {
 export const WeatherInfo = ({ weatherInfo }: Props) => {
   return (
     <>
-      {weatherInfo && (
+      {weatherInfo ? (
         <TableContainer border="solid #e2e8f0 1px" mt="20px">
           <Table variant="simple" size="sm">
-            <TableCaption fontSize="12px">今日の天気</TableCaption>
+            <TableCaption fontSize="12px">
+              <Flex justifyContent="center" alignItems="center">
+                <Box display="flex" alignItems="center" mr="5px">
+                  <SunIcon boxSize="18px" />
+                </Box>
+                <Text>今日の天気</Text>
+              </Flex>
+            </TableCaption>
             <Thead>
               <Tr backgroundColor="gray.200">
                 <Th fontSize="12px">項目</Th>
@@ -58,6 +76,10 @@ export const WeatherInfo = ({ weatherInfo }: Props) => {
             </Tbody>
           </Table>
         </TableContainer>
+      ) : (
+        <Box display="flex" justifyContent="center">
+          <SpinnerIcon boxSize="36px" mt="65px" animation={spinAnimation} />
+        </Box>
       )}
     </>
   );
